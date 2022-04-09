fecb389dccd73dd28af5685dee2c64b51daefb66
Jerry Zhang <27578913+jzhang12@users.noreply.github.com>
2022-04-08 20:47:39 -0400

    added frontend code

---
+++ components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } 
+    from "./NavbarElements";
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+        <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/login" activeStyle>
+            Log In
+          </NavLink>
+          <NavLink to="/signup" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
---
+++ pages/login_folder/login.js
@@ -0,0 +1,14 @@
+import React, { useState} from "react";
+
+
+const Login = () => {
+  return (
+    <div>
+      <h1>
+      If you already have an account, log in here:
+      </h1>
+    </div>
+  );
+};
+  
+export default Login;
