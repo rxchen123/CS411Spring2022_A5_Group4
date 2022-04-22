4405605e52a09d1792f9c8de3912485d669f8861
Roy Chen <rxchen123@gmail.com>
2022-04-22 10:41:17 -0400

    rearange input box

--- frontend/src/pages/currency.js
+++ frontend/src/pages/currency.js
@@ -6,44 +6,64 @@ const Currency = () => {
       <h1>
       Currency Converter
       </h1>
-      <form align="center">
-          <label>
-              From
-          </label>
-          <select>
-              <option value="(USD)"> US dollar (USD) </option>
-              <option value="(EUR)"> Euro (EUR) </option>
-              <option value="(CNH)"> Chinese renminbi (CNH) </option>
-          </select>
-      </form>
-      <form align="center">
-          <label>
-              Input Amount
-          </label>
-          <input
-            type = 'integer'
-            required
-          />
-      </form>
-      <form align="center"> 
-          <label>
-              To
-          </label>
-          <select>
-              <option value="(USD)"> US dollar (USD) </option>
-              <option value="(EUR)"> Euro (EUR) </option>
-              <option value="(CNH)"> Chinese renminbi (CNH) </option>
-          </select>
-      </form>
-      <form align="center">
-          <label>
-              Converted Amount
-          </label>
-          <input
-            type = 'integer'
-            required
-          />
-      </form>
+      <Container>
+        <Row>
+            <Col>
+                <form align="center">
+                    <label>
+                        From
+                    </label>
+                    <select>
+                        <option value="(USD)"> US dollar (USD) </option>
+                        <option value="(EUR)"> Euro (EUR) </option>
+                        <option value="(CNH)"> Chinese renminbi (CNH) </option>
+                    </select>
+                </form>
+            </Col>
+            
+            <Col>
+                <form align="center">
+                    <label>
+                      Input Amount
+                    </label>
+                    <input
+                        type = 'integer'
+                        required
+                    />
+                </form>
+            </Col>
+        </Row>
+        </Container>
+
+        <Container>
+            <Row>
+            <Col>
+                <form align="center"> 
+                     <label>
+                       To
+                    </label>
+                    <select>
+                        <option value="(USD)"> US dollar (USD) </option>
+                        <option value="(EUR)"> Euro (EUR) </option>
+                        <option value="(CNH)"> Chinese renminbi (CNH) </option>
+                    </select>
+                </form>
+            </Col>
+
+            <Col>
+                <form align="center">
+                    <label>
+                        Converted Amount
+                    </label>
+                    <input
+                        type = 'integer'
+                        required
+                    />
+                </form>
+            </Col>
+            </Row>
+        </Container>
+      
     </div>
   );
 };
