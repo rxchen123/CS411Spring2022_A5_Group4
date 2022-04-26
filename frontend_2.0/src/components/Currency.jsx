af5675a4d805f6b20838d1d88a2cddcde8d5d21a
Roy Chen <rxchen123@gmail.com>
2022-04-26 15:58:21 -0400

    adding some input/output container

--- frontend_2.0/src/components/Currency.jsx
+++ frontend_2.0/src/components/Currency.jsx
@@ -3,64 +3,71 @@ import React from 'react';
 function Currency() {
   return (
     <div className="Currency">
+      
       <div align="center">
-            <div class="col-lg-5">
-              <h1 class="font-weight-light">Currency Converter</h1>
-              <p>
-                Here is where you convert currency
-              </p>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Currency Converter</h1>
+            <p>
+              Here is where you convert currency
+            </p>
+          </div>
+        </div>
+    
+      <br/><br/>
+ 
+      <div class="container" align="center">
+        <v-row>
+
+          <v-col align="center">
+            <form align="center">
+              <label>
+                From:
+              </label>
+              <select>
+                <option value="(USD)"> US dollar (USD) </option>
+                <option value="(EUR)"> Euro (EUR) </option>
+                <option value="(CNH)"> Chinese renminbi (CNH) </option>
+              </select>
+            </form>
+            <form align="center">
+              <label>
+                Input Amount:
+              </label>
+              <input
+                type = 'integer'
+                required
+              />
+            </form>
+          </v-col>
+
+        <br/>
+        <br/>
+
+          <v-col align="center">
+
+            <form align="center">
+              <label>
+                To:
+              </label>
+              <select>
+                <option value="(USD)"> US dollar (USD) </option>
+                <option value="(EUR)"> Euro (EUR) </option>
+                <option value="(CNH)"> Chinese renminbi (CNH) </option>
+              </select>
+            </form>
+            <form align="center">
+              <label>
+                Output Amount:
+              </label>
+              <input
+                type = 'integer'
+                required
+              />
+            </form>
               
-            </div>
-          </div>
-
-      <div class="container">
-          <div class="col-lg-6">
-            
-            <div class="row-lg-7">
-              <form align="center">
-                <label>
-                  From
-                </label>
-                <select>
-                  <option value="(USD)"> US dollar (USD) </option>
-                  <option value="(EUR)"> Euro (EUR) </option>
-                  <option value="(CNH)"> Chinese renminbi (CNH) </option>
-                </select>
-              </form>
-              <form align="center">
-                <label>
-                  Input Amount
-                </label>
-                <input
-                  type = 'integer'
-                  required
-                />
-              </form>
-            </div>
-
-            <div class="row-lg-7">
-              <form align="center">
-                <label>
-                  To
-                </label>
-                <select>
-                  <option value="(USD)"> US dollar (USD) </option>
-                  <option value="(EUR)"> Euro (EUR) </option>
-                  <option value="(CNH)"> Chinese renminbi (CNH) </option>
-                </select>
-              </form>
-              <form align="center">
-                <label>
-                  Output Amount
-                </label>
-                <input
-                  type = 'integer'
-                  required
-                />
-              </form>
-            </div>
+          </v-col>  
           
-          </div>
+        </v-row>
           
       
       </div>
