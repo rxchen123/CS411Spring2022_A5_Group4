78d35075d41e869f27efe5c84947e2bf7ebb69ca
Stephanie Liu <stephanieliu@dhcp-wifi-8021x-155-41-86-68.bu.edu>
2022-04-26 14:53:19 -0400

    sign in form

--- frontend_2.0/src/components/Signin.jsx
+++ frontend_2.0/src/components/Signin.jsx
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-function Contact() {
+function Signin() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
   return (
     <div className="signin">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
+            <div className="app">
+            <div className="login-form">
+              <div className="title">Sign In</div>
+              {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            </div>
+          </div>
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
@@ -27,4 +106,6 @@ function Contact() {
   );
 }
 
-export default Contact;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Signin />, rootElement);
+export default Signin;
