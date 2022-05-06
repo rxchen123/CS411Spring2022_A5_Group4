f24b5e39959e61ad64174ec94743a0aa53156594
AngeloFacciponti-Mennella <agfacc@bu.edu>
2022-05-05 23:38:22 -0400

    Made Api call funcions and flask to display hotel search

---
+++ ApiCalls.py
@@ -0,0 +1,50 @@
+import requests
+
+apiKey = "9lfXz1GdokFlp1ea7h4yUP5sShrGeyUF"
+apiSecret = "uI0lNTOtKcevuBEG"
+accessToken = ""
+
+def getAccessToken():
+    """Generate an access token with a POST request to Amadeus Auth server"""
+    headers={"Content-Type": "application/x-www-form-urlencoded"}
+    data = {"grant_type":"client_credentials","client_id":apiKey,"client_secret":apiSecret}
+    response = requests.post("https://test.api.amadeus.com/v1/security/oauth2/token", headers=headers, data=data)
+    response = response.json()
+    response = response.get("access_token")
+    return response
+
+
+def latLon(city):
+    url="http://api.positionstack.com/v1/forward"
+    params = {"access_key":"15d82902cde69dd5818d355a57298704", "query":city}
+    response = requests.request("GET", url, params=params)
+    response = response.json().get("data")[0]
+    return response
+
+
+
+def hotelSearch(numAdults, checkinDate, checkoutDate, city):
+    """Perform HTTP request for hotels in an area based on form data"""
+
+    coords = latLon(city)
+    lat = coords.get("latitude")
+    lon = coords.get("longitude")
+
+    url = "https://hotels-com-provider.p.rapidapi.com/v1/hotels/nearby"
+    querystring = {"locale":"en_US","adults_number":numAdults,"checkin_date":checkinDate,"sort_order":"STAR_RATING_HIGHEST_FIRST",
+                "checkout_date":checkoutDate,"longitude":lon,"currency":"USD","latitude":lat}
+    headers = {
+	"X-RapidAPI-Host": "hotels-com-provider.p.rapidapi.com",
+	"X-RapidAPI-Key": "20192985damsh3bb2f7d1cb25fcap1588dejsn5ccf9a34cb3b"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response=response.json().get("searchResults").get("results")[:5]    #first 5 results based on criteria
+    hotels = []
+    for hotel in response:
+        hotelData = {"name":hotel.get('name'), "address" :hotel.get('address').get('streetAddress'), "price_per_night":hotel.get("ratePlan").get("price").get("current")}
+        hotels.append(hotelData)
+    
+    print(hotels)
+
+if __name__ == "__main__":
+    hotelSearch("2","2023-04-25","2023-05-06","Malibu")
--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-from msilib.schema import Directory
-from flask import Flask,render_template, send_from_directory
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS
-from api.HelloApiHandler import HelloApiHandler
+import flask
+from flask import Flask
+import ApiCalls
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app)
-api=Api(app)
+app = Flask(__name__)
 
 
-@app.route('/', defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+@app.route('/')
+def index():
+    return '''<h1>Welcome to the home page! Not much going on here...</h1>'''
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+@app.route('/hotelsearch', methods=['GET', 'POST'])
+def searchHotels():
+    if flask.request.method == 'GET':    
+        return '''
+                <h3>Search for the top 5 hotels in any US city!</h3>
+            	<form action='login' method='POST'>
+				    <input type='text' name='city' id='city' placeholder='city'></input>
+                    <p>Enter the dates in the form YYYY-MM-DD
+				    <input type='text' name='check-in Date' id='checkinDate' placeholder='checkinDate'></input>
+                    <input type='text' name='check-out Date' id='checkoutDate' placeholder='checkoutDate'></input>
+   				    <input type='text' name='Number of Adults' id='numAdults' placeholder='numAdults'></input>
+
+				    <input type='submit' name='submit' value='Search!'></input>
+			   </form></br>
+            '''
+    
+    city = flask.request.form['city']        
+    checkinDate = flask.request.form['checkinDate']
+    checkoutDate = flask.request.form['checkoutDate']
+    numAdults = flask.request.form['numAdults']
+    return ApiCalls.hotelSearch(numAdults,checkinDate,checkoutDate,city)
+        
+        # need to display a simple form, then the results once the form is submitted
+
 
 
 
